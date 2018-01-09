f05d6ddc7c0962fa49c2754bf271d3437e55e4f2
Tianon Gravi <admwiggin@gmail.com>
2018-01-09 14:59:05 -0800

    Add tmux.conf and inputrc

--- install.sh
+++ install.sh
@@ -8,3 +8,15 @@ sed -ri 's!^(HIST[A-Z]*SIZE=)!#\1!' "$HOME/.bashrc"
 if ! grep -q "$thisDir" "$HOME/.bashrc"; then
 	echo $'\n'"source '$thisDir/bashrc'" >> "$HOME/.bashrc"
 fi
+
+touch "$HOME/.tmux.conf"
+if ! grep -q "$thisDir" "$HOME/.tmux.conf"; then
+	[ -s "$HOME/.tmux.conf" ] && echo >> "$HOME/.tmux.conf"
+	echo "source-file '$thisDir/tmux.conf'" >> "$HOME/.tmux.conf"
+fi
+
+touch "$HOME/.inputrc"
+if ! grep -q "$thisDir" "$HOME/.inputrc"; then
+	[ -s "$HOME/.inputrc" ] && echo >> "$HOME/.inputrc"
+	echo "\$include $thisDir/inputrc" >> "$HOME/.inputrc"
+fi
