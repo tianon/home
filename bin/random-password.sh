5b0d67131a94a3ffaf3eb52977c2c5c61641005a
Tianon Gravi <admwiggin@gmail.com>
2018-01-12 10:36:49 -0800

    Add some stats to "random-password.sh"

--- bin/random-password.sh
+++ bin/random-password.sh
@@ -1,6 +1,18 @@
 #!/usr/bin/env bash
 set -Eeuo pipefail
 
+# $ sudo apt install 'wamerican.*' 'wbritish.*'
+# $ cat /usr/share/dict/* | sort -u | wc -l
+# 663022
+# 664557
+# 667133
+# (three different machines -- average ~665000)
+
+# > Math.pow(665000, 4) * Math.pow(16, 40)
+# 2.8581557253970005e+71
+# > Math.log2(Math.pow(665000, 4) * Math.pow(16, 40))
+# 237.37197926020255
+
 IFS=$'\n'
 randomWords=( $(cat /usr/share/dict/* | shuf -n4) )
 unset IFS
