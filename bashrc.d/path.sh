301e8e3c35ac22985a653ab02fa59c44c5c32297
Tianon Gravi <admwiggin@gmail.com>
2021-06-24 16:00:53 -0700

    Fix (and rename) PATH munging script

--- bashrc.d/path.sh
+++ bashrc.d/path.sh
@@ -1,12 +1,13 @@
 # https://github.com/tianon/debian-bin
 debianBinDir="$HOME/docker/debian-bin"
+export PATH="$PATH:$debianBinDir/generic:$debianBinDir/docker-sbuild"
+unset debianBinDir
+
 # https://github.com/tianon/docker-bin
 dockerBinDir="$HOME/docker/bin"
-export PATH="$PATH:$debianBinDir/generic:$debianBinDir/docker:$dockerBinDir:$dockerBinDir/sbuild"
-
+export PATH="$PATH:$dockerBinDir:$dockerBinDir/sbuild"
 # https://github.com/tianon/docker-museum
 if [ -r "$dockerBinDir/smart/.bashrc" ]; then
 	source "$dockerBinDir/smart/.bashrc"
 fi
-
-unset dockerBinDir debianBinDir
+unset dockerBinDir
