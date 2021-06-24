ee23b7890a4f63864c2ff270315207f2a7690494
Tianon Gravi <admwiggin@gmail.com>
2021-06-24 15:58:10 -0700

    Add "debian-bin" to PATH

--- bashrc.d/docker-bin.sh
+++ bashrc.d/docker-bin.sh
@@ -1,10 +1,12 @@
+# https://github.com/tianon/debian-bin
+debianBinDir="$HOME/docker/debian-bin"
 # https://github.com/tianon/docker-bin
 dockerBinDir="$HOME/docker/bin"
-export PATH="$PATH:$dockerBinDir:$dockerBinDir/sbuild"
+export PATH="$PATH:$debianBinDir/generic:$debianBinDir/docker:$dockerBinDir:$dockerBinDir/sbuild"
 
 # https://github.com/tianon/docker-museum
 if [ -r "$dockerBinDir/smart/.bashrc" ]; then
 	source "$dockerBinDir/smart/.bashrc"
 fi
 
-unset dockerBinDir
+unset dockerBinDir debianBinDir
