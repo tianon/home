40c53bc690b6f9922a37d0e58cf0c0788edee227
Tianon Gravi <admwiggin@gmail.com>
2025-12-09 20:58:22 -0800

    Add `updig` alias that in hindsight should've been way more obvious
    Thanks Jon 🫶

--- bashrc.d/aliases.sh
+++ bashrc.d/aliases.sh
@@ -1,4 +1,5 @@
 alias updog='docker run -it --rm -e TERM'
+alias updig='updog tianon/network-toolbox dig' # thanks Jon 🫶
 alias docker-follow='docker attach --no-stdin --sig-proxy=false'
 
 # 💕 for Clint (https://salsa.debian.org/debian/debianutils/-/commit/3a8dd10b4502f7bae8fc6973c13ce23fc9da7efb)
